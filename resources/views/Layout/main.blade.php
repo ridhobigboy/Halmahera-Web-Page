92cafa0af4872758011aa67f16655a1b4b8f9f18
ridho fadhillah <fadhillah056@gmail.com>
2022-10-01 00:04:01 +0700

    Update pagi ridho

--- resources/views/Layout/main.blade.php
+++ resources/views/Layout/main.blade.php
@@ -52,12 +52,11 @@
                     <div class="col-lg-8">
                         <h2 class="text-white mb-4">Siapa Kami</h2>
                         <p class="text-white-50">
-                            Mapala halmahera adalah Unit Kegiatan mahasiswa Sekolah tinggi Teknologi Terpadu nurul fikri, yang berfokus pada kegiatan alam bebas
-                            dengan tujuan edukasi dan juga kemanusiaan
+                            Mapala Halmahera merupakan unit kegiatan mahasiswa yang bertujuan untuk mewadahi minat dan bakat mahasiswa dalam berkegiatan dialam bebas
                         </p>
                     </div>
                 </div>
-                <img class="img-fluid" src="{{asset('Template/assets/img/perjalanan.jpg')}}" alt="..." />
+                <img class="img-fluid" src="{{asset('Template/assets/img/ipad.png')}}" alt="..." />
             </div>
         </section>
         <!-- Projects-->
@@ -65,23 +64,23 @@
             <div class="container px-4 px-lg-5">
                 <!-- Featured Project Row-->
                 <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
-                    <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="{{asset('Template/assets/img/bg-masthead.jpg')}}" alt="..." /></div>
+                    <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="{{asset('Template/assets/img/maps.jpg')}}" alt="..." /></div>
                     <div class="col-xl-4 col-lg-5">
                         <div class="featured-text text-center text-lg-left">
                             <h4>Tentang</h4>
-                            <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!</p>
+                            <p class="text-black-50 mb-0">Halmahera merupakan satu satunya unit kegiatan mahasiswa yang berfokus pada kegiatan alam bebas </p>
                         </div>
                     </div>
                 </div>
                 <!-- Project One Row-->
                 <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
-                    <div class="col-lg-6"><img class="img-fluid" src="{{asset('Template/assets/img/demo-image-01.jpg')}}" alt="..." /></div>
+                    <div class="col-lg-6"><img class="img-fluid" src="{{asset('Template/assets/img/makanan-survival.jpg')}}" alt="..." /></div>
                     <div class="col-lg-6">
                         <div class="bg-black text-center h-100 project">
                             <div class="d-flex h-100">
                                 <div class="project-text w-100 my-auto text-center text-lg-left">
-                                    <h4 class="text-white">Misty</h4>
-                                    <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
+                                    <h4 class="text-white">Fokus</h4>
+                                    <p class="mb-0 text-white-50">Mapala Halmahera untuk saat ini berfokus pada kegiatan Gunung Hutan dan segala hal yang berkaitan dengan nya.</p>
                                     <hr class="d-none d-lg-block mb-0 ms-0" />
                                 </div>
                             </div>
@@ -90,13 +89,13 @@
                 </div>
                 <!-- Project Two Row-->
                 <div class="row gx-0 justify-content-center">
-                    <div class="col-lg-6"><img class="img-fluid" src="{{asset('Template/assets/img/demo-image-02.jpg')}}" alt="..." /></div>
+                    <div class="col-lg-6"><img class="img-fluid" src="{{asset('Template/assets/img/navdar.jpg')}}" alt="..." /></div>
                     <div class="col-lg-6 order-lg-first">
                         <div class="bg-black text-center h-100 project">
                             <div class="d-flex h-100">
                                 <div class="project-text w-100 my-auto text-center text-lg-right">
                                     <h4 class="text-white">Mountains</h4>
-                                    <p class="mb-0 text-white-50">Another example of a project with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>
+                                    <p class="mb-0 text-white-50">Kegiatan Mapala Halmahera bertujuan untuk memberikan efek baik pada semua orang yang terlibat di dalam nya</p>
                                     <hr class="d-none d-lg-block mb-0 me-0" />
                                 </div>
                             </div>
